455f180bb047f2644ba5fd7339e9d132aafc8bcc
Benjamin Darcet <b.darcet@gmail.com>
2018-11-21 13:59:09 +0100

    yelp done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '00 00 00 00 01',
+    category:     'chinese',
+  },
+  {
+    name:         'Sushi pasha',
+    address:      '50 Tokyo St, London E2 7JE',
+    phone_number: '00 00 00 00 02',
+    category:     'japanese',
+  },
+  {
+    name:         'London restaurant',
+    address:      '20 London St',
+    phone_number: '00 00 00 00 03',
+    category:     'french',
+  },
+  {
+    name:         'Super restaurant',
+    address:      '5 belgium St',
+    phone_number: '00 00 00 00 04',
+    category:     'belgian',
+  },
+  {
+    name:         'pizza restaurant',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '00 00 00 00 05',
+    category:     'italian',
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+
+puts 'Finished!'
+
